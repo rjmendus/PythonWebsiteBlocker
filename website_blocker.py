a2a191dc2b4903dbfecdba199904176e3656f3ed
Rajat Mendus <jaicmendus@gmail.com>
2018-07-06 10:55:07 +0530

    Setting the basic stuff

---
+++ website_blocker.py
@@ -0,0 +1,8 @@
+# Path to the hosts file in Windows
+hosts_path = r"C:\Windows\System32\drivers\etc\hosts"
+
+# Holding values to be entered in the hosts file. Include 'www' included url too
+redirect = "127.0.0.1"
+website_list = ["www.facebook.com","facebook.com"]
+
+# Blocking websites from 8 am to 4 pm
