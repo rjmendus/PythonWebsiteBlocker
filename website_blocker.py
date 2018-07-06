c59368c33496f340c144131127c78b11ea2989d7
Rajat Mendus <jaicmendus@gmail.com>
2018-07-06 11:19:09 +0530

    Add the checking for working hours

--- website_blocker.py
+++ website_blocker.py
@@ -1,3 +1,6 @@
+import time
+from datetime import datetime as dt
+
 # Path to the hosts file in Windows
 hosts_path = r"C:\Windows\System32\drivers\etc\hosts"
 
@@ -6,3 +9,8 @@ redirect = "127.0.0.1"
 website_list = ["www.facebook.com","facebook.com"]
 
 # Blocking websites from 8 am to 4 pm
+
+while (True):
+	if dt(dt.now().year,dt.now().month,dt.now().day,8) < dt.now() < dt(dt.now().year,dt.now().month,dt.now().day,16):
+		print("Working hours")
+		time.sleep(5)
