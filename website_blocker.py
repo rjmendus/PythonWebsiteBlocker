09d6810a3a33a2d4411245f71f53cee85a8cc92a
Rajat Mendus <jaicmendus@gmail.com>
2018-07-06 11:39:56 +0530

    Add website name to hosts file

--- website_blocker.py
+++ website_blocker.py
@@ -3,6 +3,7 @@ from datetime import datetime as dt
 
 # Path to the hosts file in Windows
 hosts_path = r"C:\Windows\System32\drivers\etc\hosts"
+hosts_temp = "hosts"
 
 # Holding values to be entered in the hosts file. Include 'www' included url too
 redirect = "127.0.0.1"
@@ -13,6 +14,13 @@ website_list = ["www.facebook.com","facebook.com"]
 while (True):
 	if dt(dt.now().year,dt.now().month,dt.now().day,8) < dt.now() < dt(dt.now().year,dt.now().month,dt.now().day,16):
 		print("Working hours...")
+		with open(hosts_temp, 'r+') as file:
+			content = file.read()
+			for website in website_list:
+				if website in content:
+					pass
+				else:
+					file.write("\n"+redirect+" "+website) 
 	else:
 		print("Fun hours...")
 	time.sleep(5)
